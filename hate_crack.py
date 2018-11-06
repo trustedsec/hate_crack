69a1b2dbced32fc199fe4a6ee1b25dc1b5fd6ae1
Jeff H <jeff@jeffh.net>
2018-11-05 19:40:25 -0800

    test remove duplicates

--- hate_crack.py
+++ hate_crack.py
@@ -1012,13 +1012,27 @@ def middle_combinator():
 
 # Remove Duplicate Accounts
 def remove_duplicate_accounts():
-    print "test\n"
+    # create copy of origional file
+    if not os.path.isfile(hcatHashFile + ".orig"):
+        shutil.copyfile(hcatHashFile, hcatHashFile + ".orig")
+    # Remove accounts ending in $
+    newfile = open(hcatHashFile + ".tmp", 'w')
+    uniqueUsers = {}
+    with open(hcatHashFile, 'r') as f:
+        for lines in f:
+            line = lines.split("::")
+            if lines[0] + "::" + lines[1] not in uniqueUsers:
+                uniqueUsers.update(line[0] + "::" + line[1])
+                newfile.write(str(line))
+
+    newfile.close()
+    shutil.move(hcatHashFile + ".tmp", hcatHashFile)
+    print "Computer Accounts Removed\n"
 
 # Remove Computer Accounts
 def remove_computer_accounts():
 #create copy of origional file
     if not os.path.isfile(hcatHashFile + ".orig"):
-        #from shutil import copyfile
         shutil.copyfile(hcatHashFile, hcatHashFile + ".orig")
 #Remove accounts ending in $
     newfile = open(hcatHashFile + ".tmp", 'w')
